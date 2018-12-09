ba7986e3d1d4c3d582aa5881d8a8d82d9b89e488
unknown <tom.martyniak@gmail.com>
2018-12-09 11:51:11 +0100

    First commit

---
+++ calc.js
@@ -0,0 +1,28 @@
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var result = document.getElementById("result");
+var form = document.getElementById("x%y");
+
+form.addEventListener('submit',function(event) {
+    //result.value = num1 / num2 * 100;
+    
+    if (!num1.value || !num2.value){
+        alert("Please eneter values");
+    } else {
+        var x = parseFloat(num1.value);
+        var y = parseFloat(num2.value);
+        
+        var percent = x / y * 100;
+        
+        result.innerText = "Answer: " + percent + "%";
+        
+        event.preventDefault();
+    }
+    
+    
+    
+});
+
+//num1.value = "iiii";
+//result.innerText = "????";
+//alert(num1.value);
